2bb281ed82b9a699d8aa7f96fe05cee717d59698
Izadora Araujo Silva <144824497+IzadoraAra@users.noreply.github.com>
2024-10-11 09:59:08 -0300

    perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,24 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Como esta tua vida atualmente?',
+    'Esta muito boa, estou muito feliz ultimamente'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
+    'Como esta sendo esse ano para voce?',
+    'Este ano esta sndo maravilhoso'
+)   
+
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Onde voce cresceu?',
+    'Em uma cidade do interior do Paraná, na cidade de Reserva PR'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Quem é seu super-herói favorito?',
+    'Mulher maravilha'
 )
